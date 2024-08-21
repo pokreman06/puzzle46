c8ca7545078784b68c852a399753c031cc9c96f5
Carl Franklin <carl@appvnext.com>
2024-08-21 10:50:24 -0400

    Add project files.

---
+++ Puzzle-46/Components/CascadingAppState.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Puzzle_46.Components;
+
+public partial class CascadingAppState : ComponentBase
+{
+    [Parameter]
+    public RenderFragment ChildContent { get; set; }
+
+    private string message = "";
+    public string Message
+    {
+        get => message;
+        set
+        {
+            message = value;
+            // Force a re-render
+            StateHasChanged();
+        }
+    }
+}
---
+++ Puzzle-46/StorageService.cs
@@ -0,0 +1,27 @@
+﻿using Puzzle_46.Components;
+
+namespace Puzzle_46;
+
+public class StorageService
+{
+    public string Message { get; set; } = "Hello from my service";
+    //private CascadingAppState _cascadingAppState;
+
+    //public StorageService(CascadingAppState cascadingAppState)
+    //{
+    //    _cascadingAppState = cascadingAppState;
+    //}
+
+    public void SetMessage(string message)
+    {
+        Message = message;
+    }
+
+    public string GetMessage()
+    {
+        // How can we access the CascadingAppState from here?
+
+        return Message;
+        //return _cascadingAppState.Message;
+    }
+}
